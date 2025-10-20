b5867d587a46e569fdc22dc8c5c0f5c821274978
hmworsham <hmworsham@gmail.com>
2025-10-20 11:29:33 -0600

    use fread for fast ingest

--- R/saferead.csv.R
+++ R/saferead.csv.R
@@ -1,6 +1,6 @@
 #'@export
 saferead.csv <- function(infile){
-  tryCatch(read.csv(infile, header=T), 
+  tryCatch(fread(infile), 
            error = function(cond) {
              message(paste('Reading csv failed'))
            })
